7a4e7d5636d6e440832503b7adbf3f57e4bc3866
Bruno Morgan <bumorgan@hotmail.com>
2023-03-20 14:03:37 -0300

    Initial Commit

---
+++ swiftui-marvel/swiftui_marvelApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_marvelApp.swift
+//  swiftui-marvel
+//
+//  Created by Bruno Morgan on 20/03/23.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_marvelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
